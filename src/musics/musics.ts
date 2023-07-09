f9f8e419db0eb478a02628c3dabfca304099d6e0
BenGregory23 <bengregorygbr@gmail.com>
2023-07-09 15:45:13 +0200

    added music

--- src/musics/musics.ts
+++ src/musics/musics.ts
@@ -14,6 +14,11 @@ const musics = [
     musicId: 'q2t5eL0XhRk',
     image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2Fq2t5eL0XhRk%2Fmaxresdefault.jpg&f=1&nofb=1&ipt=22a2d190bdb7c5c23bc4b434d01ca0c50bcacf702f6997a7150a9b7ee567d095&ipo=images'
   },
+  {
+    name: 'Synthwave',
+    musicId: 'y2ECgOhoDGs',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2Fy2ECgOhoDGs%2Fmaxresdefault.jpg&f=1&nofb=1&ipt=22a2d190bdb7c5c23bc4b434d01ca0c50bcacf702f6997a7150a9b7ee567d095&ipo=images'
+  }
 ]
 
 export default musics
